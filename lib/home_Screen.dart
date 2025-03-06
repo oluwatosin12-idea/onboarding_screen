97cc399318d82e1b3c7de48540bf7b1f2179e21f
Oluwatosin Titilola <130060850+oluwatosin12-idea@users.noreply.github.com>
2025-03-06 02:13:57 +0100

    continuation

--- lib/home_Screen.dart
+++ lib/home_Screen.dart
@@ -9,7 +9,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool? isChecked = false; // State variable for checkbox
+  bool isAppleChecked = false;
+  bool isMangoChecked = false;
+  bool isBananaChecked = false;
+
+  // String? selectedItem;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,14 +68,22 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    Padding(
+                        padding: EdgeInsets.symmetric(
+                      horizontal: 5,
+                      vertical: 15,
+                    )),
                     Text(
                       '28 May',
                       style: TextStyle(
                           fontWeight: FontWeight.w400,
                           fontSize: 16,
                           color: Colors.white70),
+                    ),
+                    SizedBox(
+                      width: 280,
                     ),
                     Icon(
                       Icons.ios_share,
@@ -84,19 +96,31 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Column(
                     children: [
                       Padding(padding: EdgeInsets.only(left: 10)),
-                      Text(
-                        'Task Management App Ui Design',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Colors.white),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(padding: EdgeInsets.only(left: 10)),
+                          Text(
+                            'Task Management App Ui Design',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20,
+                                color: Colors.white),
+                          ),
+                        ],
                       ),
-                      Text(
-                        'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used.',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white70),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: Text(
+                          'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used.',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white70),
+                        ),
                       ),
                       SizedBox(
                         height: 15,
@@ -127,9 +151,9 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
-          SizedBox(height: 20,),
+          SizedBox(height: 20),
           Container(
-            height: 188,
+            height: 219,
             width: 388,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(9),
@@ -138,8 +162,13 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    Padding(
+                        padding: EdgeInsets.symmetric(
+                      horizontal: 5,
+                      vertical: 15,
+                    )),
                     Text(
                       '12 May',
                       style: TextStyle(
@@ -147,68 +176,114 @@ class _HomeScreenState extends State<HomeScreen> {
                           fontSize: 16,
                           color: Colors.white70),
                     ),
+                    SizedBox(
+                      width: 280,
+                    ),
                     Icon(
                       Icons.ios_share,
                       color: Colors.white,
                     )
                   ],
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Column(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 10)),
-                      Text(
-                        'Shopping List',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Colors.white),
-                      ),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Checkbox(
-                                  value: isChecked,
-                                  onChanged: (bool? value) {
-                                    setState(() {
-                                      isChecked = value ?? false;
-                                    });
-                                  }),
-                              Text(
-                                'Apple',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 16,
-                                    color: Colors.white70),
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                  value: isChecked,
-                                  onChanged: (bool? value) {
-                                    setState(() {
-                                      isChecked = value ?? true;
-                                    });
-                                  }),
-                              Text(
-                                'Mango Juice',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 16,
-                                    color: Colors.white70),
-                              )
-                            ],
-                          ),
-
-                        ],
-                      )
-                    ],
-                  ),
+                SizedBox(
+                  height: 10,
                 ),
+                Column(
+                  children: [
+                    //Padding(padding: EdgeInsets.only(right: 10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 10)),
+                        Text(
+                          'Shopping List',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                                value: isAppleChecked,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    isAppleChecked = value ?? false;
+                                  });
+                                }),
+                            Text(
+                              'Apple',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16,
+                                  color: Colors.white70),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(
+                                value: isMangoChecked,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    isMangoChecked = value ?? false;
+                                  });
+                                }),
+                            Text(
+                              'Mango Juice',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16,
+                                  color: Colors.white70),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(
+                                value: isBananaChecked,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    isBananaChecked = value ?? false;
+                                  });
+                                }),
+                            Text(
+                              'Banana 5 pcs with ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16,
+                                  color: Colors.white70),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 90),
+          Container(margin: EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.view_headline,
+                  color: Colors.white,
+                ),
+                FloatingActionButton(
+                  backgroundColor: Color(0xFF5F5AC9),
+                  foregroundColor: Color(0xFFFFFFFF),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80)),
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                )
               ],
             ),
           )
